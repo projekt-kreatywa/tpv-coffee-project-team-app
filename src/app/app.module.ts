4f02a67a0e2fd6e390fcdbcd8efa90a1ebdeb25f
@rafszul <rafszul@gmail.com>
2017-01-16 23:36:45 +0000

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from '../app/config/firebase.config';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +20,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FlexLayoutModule.forRoot(),
+    MaterialModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/config/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBCzOUN7-paczfuxP1y_iLAvai_aDwnb3g',
+    authDomain: 'tpv-coffee-project-team-app.firebaseapp.com',
+    databaseURL: 'https://tpv-coffee-project-team-app.firebaseio.com',
+    storageBucket: 'tpv-coffee-project-team-app.appspot.com',
+    messagingSenderId: '693774969155'
+};
