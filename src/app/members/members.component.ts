819dfff0a16436471238ad9287532e6083c0a554
@rafszul <rafszul@gmail.com>
2017-01-17 05:48:37 +0000

    4 initial auth components

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { Router } from '@angular/router';
+import { moveIn, fallIn, moveInLeft } from '../router.animations';
+
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.scss']
+  styleUrls: ['./members.component.scss'],
+  animations: [moveIn(), fallIn(), moveInLeft()],
+  host: { '[@moveIn]': '' }
 })
 export class MembersComponent implements OnInit {
 
-  constructor() { }
+  name: any;
+  state: string = '';
+
+  constructor(public af: AngularFire, private router: Router) {
+
+    this.af.auth.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+
+  }
+
+  logout() {
+    this.af.auth.logout();
+    console.log('logged out');
+    this.router.navigateByUrl('/login');
+  }
+
 
   ngOnInit() {
   }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+import { Router } from '@angular/router';
+import { moveIn, fallIn } from '../router.animations';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
+  animations: [moveIn(), fallIn()],
+  host: {
+    '[@moveIn]': ''
+  }
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  state: string = '';
+  error: any;
+
+  constructor(public af: AngularFire, private router: Router) {
+
+  }
 
   ngOnInit() {
   }
 
-}
+  onSubmit(formData) {
+    if (formData.valid) {
+      console.log(formData.value);
+      this.af.auth.createUser({
+        email: formData.value.email,
+        password: formData.value.password
+      }).then(
+        (success) => {
+          console.log(success);
+          this.router.navigate(['/login'])
+        }).catch(
+        (err) => {
+          console.log(err);
+          this.error = err;
+        });
+    }
+  }
+
