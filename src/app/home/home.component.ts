ae5d954d6bd350f84637e21f30d974ff1d64ee86
rafszul <rafszul@gmail.com>
2017-01-29 07:48:56 +0000

    app works

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,8 +5,8 @@ import { moveIn, fallIn, moveInLeft } from '../router.animations';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
-  // animations: [moveIn(), fallIn(), moveInLeft()],
+  styleUrls: ['./home.component.scss'],
+  animations: [moveIn(), fallIn(), moveInLeft()]
   // host: { '[@moveIn]': '' }
 })
 export class HomeComponent implements OnInit {
